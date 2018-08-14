886cf7f631b2d036140eae17294faa0bb02bfa61
Aaron <ach5910@gmail.com>
2018-08-14 13:54:57 -0700

    Added startSetExpenses action and reducers

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import  getVisibleExpenses  from './selectors/expenses';
 import './firebase/firebase';
@@ -14,12 +14,13 @@ const store = configureStore();
 
 store.subscribe(() => {
     const state = store.getState();
+    console.log('state', state);
     const visisbleExpenses = getVisibleExpenses(state.expenses, state.filters)
     console.log(visisbleExpenses);
 });
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 45000}));
-store.dispatch(addExpense({ description: 'Rent', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Water Bill', amount: 109500}));
+// store.dispatch(addExpense({ description: 'Gas Bill', amount: 45000}));
+// store.dispatch(addExpense({ description: 'Rent', createdAt: 1000}));
+// store.dispatch(addExpense({ description: 'Water Bill', amount: 109500}));
 // store.dispatch(setTextFilter('bill'));
 // store.dispatch(setTextFilter('water'));
 
@@ -29,6 +30,9 @@ const jsx = (
     </Provider>
     
 );
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+})
 
-ReactDOM.render(jsx, document.getElementById('app'));
